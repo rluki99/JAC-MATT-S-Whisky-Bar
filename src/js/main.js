b8d11a860a9cf080cb92bc985f2633173ced7580
rluki99 <120097849+rluki99@users.noreply.github.com>
2023-03-23 18:20:22 +0100

    Form validation in JS added

--- src/js/main.js
+++ src/js/main.js
@@ -5,9 +5,18 @@ const navLinks = document.querySelectorAll('.nav__link')
 const footerYear = document.querySelector('.year')
 const navWrapper = document.querySelector('.wrapper-nav')
 const html = document.documentElement
-// const allATags = document.querySelectorAll('a')
 let lastScrollY = window.scrollY
 let lastWidthX = window.innerWidth
+//form validation
+const names = document.querySelector('#name')
+const mail = document.querySelector('#mail')
+const phone = document.querySelector('#phone')
+const eventData = document.querySelector('#data')
+const eventPlace = document.querySelector('#place')
+const msg = document.querySelector('#msg')
+const sendBtn = document.querySelector('.contact__form-btn')
+const form = document.querySelector('.contact__form')
+const honeypot = document.querySelector('#honeypot')
 
 const hamburgerActive = () => {
 	navBtn.classList.toggle('is-active')
@@ -20,6 +29,84 @@ const hamburgerActive = () => {
 			body.classList.remove('no-scroll-mobile')
 		})
 	})
+}
+
+//form validation
+
+const checkName = () => {
+	const re = /^[\s\p{L}]+$/u
+
+	if (re.test(names.value)) {
+		names.classList.remove('form-input--error')
+	} else {
+		names.classList.add('form-input--error')
+	}
+}
+
+const checkMail = () => {
+	const re =
+		/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,10}))$/
+
+	if (re.test(mail.value)) {
+		mail.classList.remove('form-input--error')
+	} else {
+		mail.classList.add('form-input--error')
+	}
+}
+
+const checkPhone = () => {
+	if (phone.value === '') {
+		phone.classList.remove('form-input--error')
+	}
+
+	const re = /^\+?[0-9]{9,11}$/
+
+	if (re.test(phone.value)) {
+		phone.classList.remove('form-input--error')
+	} else {
+		phone.classList.add('form-input--error')
+	}
+}
+
+const checkDate = () => {
+	if (eventData.value) {
+		eventData.classList.remove('form-input--error')
+	} else {
+		eventData.classList.add('form-input--error')
+	}
+}
+
+const checkPlace = () => {
+	if (eventPlace.value) {
+		eventPlace.classList.remove('form-input--error')
+	} else {
+		eventPlace.classList.add('form-input--error')
+	}
+}
+
+const checkMsg = () => {
+	if (msg.value) {
+		msg.classList.remove('form-input--error')
+	} else {
+		msg.classList.add('form-input--error')
+	}
+}
+
+//bot spam trap
+const checkHoneypot = () => {
+	if (honeypot.value) {
+		return false
+	}
+	return true
+}
+
+const resetForn = () => {
+	names.value = ''
+	mail.value = ''
+	phone.value = ''
+	eventData.value = ''
+	eventPlace.value = ''
+	msg.value = ''
 }
 
 //get currentyear in the footer
@@ -54,25 +141,24 @@ window.addEventListener('scroll', () => {
 })
 
 //direction-dependent scroll-padding on navigation
-navLinks.forEach(link => {
-	link.addEventListener('click', function(event) {
-		event.preventDefault();
-		const target = this.getAttribute('href');
-		const targetElement = document.querySelector(target);
-		const targetPosition = targetElement.offsetTop;
-		const currentPosition = window.scrollY;
-		const direction = targetPosition < currentPosition ? 'up' : 'down';
-	
-		if (direction === 'down') {
-		  html.style.scrollPaddingTop = '0';
-		} else {
-		  html.style.scrollPaddingTop = navWrapper.offsetHeight + 'px';
-		}
-	
-		document.querySelector(target).scrollIntoView({ behavior: 'smooth' });
-	  });
-})
+navLinks.forEach((link) => {
+	link.addEventListener('click', function (event) {
+		event.preventDefault()
+		const target = this.getAttribute('href')
+		const targetElement = document.querySelector(target)
+		const targetPosition = targetElement.offsetTop
+		const currentPosition = window.scrollY
+		const direction = targetPosition < currentPosition ? 'up' : 'down'
 
+		if (direction === 'down') {
+			html.style.scrollPaddingTop = '0'
+		} else {
+			html.style.scrollPaddingTop = navWrapper.offsetHeight + 'px'
+		}
+
+		document.querySelector(target).scrollIntoView({ behavior: 'smooth' })
+	})
+})
 
 //remove open mobile nav after dekstop breakpoint
 window.addEventListener('resize', () => {
@@ -82,5 +168,30 @@ window.addEventListener('resize', () => {
 		navBtn.classList.remove('is-active')
 		menu.classList.remove('nav__links--active')
 		body.classList.remove('no-scroll-mobile')
+	}
+})
+
+//form validation send if no errors
+sendBtn.addEventListener('click', (e) => {
+	e.preventDefault()
+
+	checkName()
+	checkMail()
+	checkPhone()
+	checkDate()
+	checkPlace()
+	checkMsg()
+
+	const isNameValid = !names.classList.contains('form-input--error')
+	const isMailValid = !mail.classList.contains('form-input--error')
+	const isPhoneValid = !phone.classList.contains('form-input--error')
+	const isDateValid = !eventData.classList.contains('form-input--error')
+	const isPlaceValid = !eventPlace.classList.contains('form-input--error')
+	const isMsgValid = !msg.classList.contains('form-input--error')
+	const isHoneypotEmpty = checkHoneypot();
+
+	if (isNameValid && isMailValid && isPhoneValid && isDateValid && isPlaceValid && isMsgValid && isHoneypotEmpty){
+		HTMLFormElement.prototype.submit.call(form)
+		resetForn()
 	}
 })
